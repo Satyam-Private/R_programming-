29d7bcf8511bb9413d2cfe2ceda4043af6cb79e0
Satyam-Private <satyampatil1252@gmail.com>
2024-08-30 21:16:31 +0530

    first program in r

---
+++ First.R
@@ -0,0 +1,6 @@
+name = readline(prompt = "enter you name")
+age = readline(prompt  = " enter your age")
+print(paste("you name is ", name , " and your age is ", age))
+
+for ( x in 1:10)
+  print(x);
